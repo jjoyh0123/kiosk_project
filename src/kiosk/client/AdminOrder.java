393223d5c9676b4b6271ad175d05f95f1fc590fc
DESKTOP-6IQMASD\user <jjoyh0123@gmail.com>
2024-11-26 16:08:06 +0900

    feature complete

---
+++ src/kiosk/client/AdminOrder.java
@@ -0,0 +1,34 @@
+package kiosk.client;
+
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class AdminOrder extends JFrame {
+	
+	JPanel topPanel, listPanel;
+	JButton refresh;
+	
+	public AdminOrder() {
+		JPanel topPanel = new JPanel();
+		refresh = new JButton("refresh");
+		topPanel.add(refresh);
+		
+		JPanel listPanel = new JPanel();
+		listPanel.setLayout(new CardLayout());
+		
+		
+	
+		
+		this.setBounds(300, 350, 280, 500);
+		this.setVisible(true);
+		
+	}
+	
+	 
+}
