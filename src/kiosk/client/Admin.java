1165094feedd519c29313837c0fc51349aca565b
Ted19horse <ted19horse@gmail.com>
2024-11-27 14:07:43 +0900

    add IntellyJ setting

--- src/kiosk/client/Admin.java
+++ src/kiosk/client/Admin.java
@@ -1,15 +1,9 @@
 package kiosk.client;
 
-import java.awt.Color;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JPanel;
 
 public class Admin extends JDialog {
   // MainFrame mainFrame;
