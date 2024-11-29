dcc5f1031a13c6139145440879a242b8febc6f75
Ted19horse <ted19horse@gmail.com>
2024-11-29 17:37:50 +0900

    241129

--- src/kiosk/adminOrderManagement/AdminOrder.java
+++ src/kiosk/adminOrderManagement/AdminOrder.java
@@ -1,27 +1,24 @@
 package kiosk.adminOrderManagement;
 
-import java.awt.CardLayout;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 
 public class AdminOrder extends JFrame {
 
-	JPanel topPanel, listPanel;
-	JButton refresh;
+  JPanel topPanel, listPanel;
+  JButton refresh;
 
-	public AdminOrder() {
-		JPanel topPanel = new JPanel();
-		refresh = new JButton("refresh");
-		topPanel.add(refresh);
+  public AdminOrder() {
+    JPanel topPanel = new JPanel();
+    refresh = new JButton("refresh");
+    topPanel.add(refresh);
 
-		JPanel listPanel = new JPanel();
-		listPanel.setLayout(new CardLayout());
+    JPanel listPanel = new JPanel();
+    listPanel.setLayout(new CardLayout());
 
-		this.setBounds(300, 350, 280, 500);
-		this.setVisible(true);
+    this.setBounds(300, 350, 280, 500);
+    this.setVisible(true);
 
-	}
+  }
 
 }
--- src/kiosk/adminVO/productVO.java
+++ src/kiosk/adminVO/productVO.java
@@ -2,77 +2,77 @@
 package kiosk.adminVO;
 
 public class productVO {
-    int idx;
-    String productName;
-    String productPrice;
-    String productCategory;
-    boolean productRecommendStatus;
-    boolean productSaleStatus;
-    String productImagePath;
-    String productRegDate;
+  int idx;
+  String productName;
+  String productPrice;
+  String productCategory;
+  boolean productRecommendStatus;
+  boolean productSaleStatus;
+  String productImagePath;
+  String productRegDate;
 
-    public String getProductRegDate() {
-        return productRegDate;
-    }
+  public String getProductRegDate() {
+    return productRegDate;
+  }
 
-    public void setProductRegDate(String productRegDate) {
-        this.productRegDate = productRegDate;
-    }
+  public void setProductRegDate(String productRegDate) {
+    this.productRegDate = productRegDate;
+  }
 
-    // Getters and Setters
-    public int getIdx() {
-        return idx;
-    }
+  // Getters and Setters
+  public int getIdx() {
+    return idx;
+  }
 
-    public void setIdx(int idx) {
-        this.idx = idx;
-    }
+  public void setIdx(int idx) {
+    this.idx = idx;
+  }
 
-    public String getProductName() {
-        return productName;
-    }
+  public String getProductName() {
+    return productName;
+  }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
 
-    public String getProductPrice() {
-        return productPrice;
-    }
+  public String getProductPrice() {
+    return productPrice;
+  }
 
-    public void setProductPrice(String productPrice) {
-        this.productPrice = productPrice;
-    }
+  public void setProductPrice(String productPrice) {
+    this.productPrice = productPrice;
+  }
 
-    public String getProductCategory() {
-        return productCategory;
-    }
+  public String getProductCategory() {
+    return productCategory;
+  }
 
-    public void setProductCategory(String productCategory) {
-        this.productCategory = productCategory;
-    }
+  public void setProductCategory(String productCategory) {
+    this.productCategory = productCategory;
+  }
 
-    public boolean isProductRecommendStatus() {
-        return productRecommendStatus;
-    }
+  public boolean isProductRecommendStatus() {
+    return productRecommendStatus;
+  }
 
-    public void setProductRecommendStatus(boolean productRecommendStatus) {
-        this.productRecommendStatus = productRecommendStatus;
-    }
+  public void setProductRecommendStatus(boolean productRecommendStatus) {
+    this.productRecommendStatus = productRecommendStatus;
+  }
 
-    public boolean isProductSaleStatus() {
-        return productSaleStatus;
-    }
+  public boolean isProductSaleStatus() {
+    return productSaleStatus;
+  }
 
-    public void setProductSaleStatus(boolean productSaleStatus) {
-        this.productSaleStatus = productSaleStatus;
-    }
+  public void setProductSaleStatus(boolean productSaleStatus) {
+    this.productSaleStatus = productSaleStatus;
+  }
 
-    public String getProductImagePath() {
-        return productImagePath;
-    }
+  public String getProductImagePath() {
+    return productImagePath;
+  }
 
-    public void setProductImagePath(String productImagePath) {
-        this.productImagePath = productImagePath;
-    }
+  public void setProductImagePath(String productImagePath) {
+    this.productImagePath = productImagePath;
+  }
 }
